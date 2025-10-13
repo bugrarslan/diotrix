b158da4bff86c1f9a8c6dfaf02d4b84628c35e74
bugrarslan <bugra.arslan7@outlook.com>
2025-10-13 17:32:12 +0300

    feat: Update app icons and splash screen assets; enhance iOS and Android icon configurations

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,24 @@
-import { Text } from "react-native";
+import { useSettingsContext } from "@/context/SettingsContext";
+import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import BackgroundStars from "@/components/ui/BackgroundStars";
+import { Image } from "react-native";
 
 export default function Index() {
+
+  const { settings } = useSettingsContext();
+  const selectedTheme = settings?.theme ?? "light";
+  const isDarkTheme = selectedTheme === "dark";
+  const backgroundColor = isDarkTheme ? 'bg-black' : 'bg-white';
   return (
-    <SafeAreaView className="items-center justify-center flex-1 bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <SafeAreaView className={`items-center justify-center flex-1 ${backgroundColor}`}>
+      <StatusBar style={isDarkTheme ? "light" : "dark"} />
+      <BackgroundStars />
+      <Image
+        source={require("@/assets/icons/splash-icon.png")}
+        style={{ resizeMode: "contain", aspectRatio: 1 }}
+        height={200}
+      />
     </SafeAreaView>
   );
 }
